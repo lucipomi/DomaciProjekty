656ed6f72c077ecc8c0ea507de430217846ba451
Lucie Pomichalkova <pomichalkova@gmail.com>
2018-04-17 13:31:42 +0200

    Pridavam domaci projekty 4b

---
+++ DomaciProjekty4b.py
@@ -0,0 +1,25 @@
+#Domaci ukol c. 11
+
+for X in range(1):
+    for j in range(1,5):
+        print("X "*j)
+    printend=("\n")
+
+
+#Domaci ukol c.12
+for radek in range(1,5):
+    if radek in range(1,2):
+        print("Prvni radek")
+    if radek in range(2,5):
+        print("Neni prvni")
+
+#Domaci ukol c.13
+for radek in range(1,7):
+    if radek in range(1,2):
+        print(6*"X ")
+    if radek in range(2,6):
+        print("X         X")
+    if radek in range(6,7):
+        print(6*"X ")
+
+#Domaci ukol c.14
